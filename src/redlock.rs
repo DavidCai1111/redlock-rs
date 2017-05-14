fd26f8da708206750cb6b891186aa9d1da00b22f
David Cai <davidcai1993@yahoo.com>
2017-05-14 15:51:13 +0800

    test: add tests for Config::default && Redlock::new

--- src/redlock.rs
+++ src/redlock.rs
@@ -26,7 +26,7 @@ pub struct Config<T: redis::IntoConnectionInfo> {
 impl Default for Config<&'static str> {
     fn default() -> Self {
         Config {
-            addrs: vec!["127.0.0.1:6379"],
+            addrs: vec!["redis://127.0.0.1/"],
             retry_count: 10,
             retry_delay: time::Duration::from_millis(400),
         }
@@ -53,5 +53,37 @@ impl Redlock {
     pub fn lock(&self, resource_name: &str, ttl: time::Duration) -> RedlockResult<()> {
         let mut waitings = self.servers.len();
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_config_default() {
+        let default_config = Config::default();
+        assert_eq!(default_config.addrs, vec!["redis://127.0.0.1/"]);
+        assert_eq!(default_config.retry_count, 10);
+        assert_eq!(default_config.retry_delay, time::Duration::from_millis(400));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_new_with_no_server() {
+        let redlock = Redlock::new::<&str>(Config {
+                                               addrs: vec![],
+                                               retry_count: 10,
+                                               retry_delay: time::Duration::from_millis(400),
+                                           })
+                .unwrap();
+    }
+
+    #[test]
+    fn test_new() {
+        let redlock = Redlock::new(Config::default()).unwrap();
+        assert_eq!(1, redlock.servers.len());
+        assert_eq!(redlock.retry_count, 10);
+        assert_eq!(redlock.retry_delay, time::Duration::from_millis(400));
     }
 }
